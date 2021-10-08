7607f893372260c0a461ac26b3ada2b03d82c87f
dash <dasha_erminski@mail.ru>
2021-10-08 19:37:54 +0300

    homework_2

---
+++ hw2/task1.py
@@ -0,0 +1,8 @@
+n1 = 15 * 3
+print(f'Result: {n1},\t\ttype: {type(n1)}')
+n2 = 15 / 3
+print(f'Result: {n2},\ttype: {type(n2)}')
+n3 = 15 // 3
+print(f'Result: {n3},\t\ttype: {type(n3)}')
+n4 = 15 ** 2
+print(f'Result: {n1},\t\ttype: {type(n4)}')
