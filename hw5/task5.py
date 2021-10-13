3d3ee0c8dad77d3bbd877c683aaf2ed9362a2fac
dash <dasha_erminski@mail.ru>
2021-10-13 19:23:34 +0300

    homework_5

---
+++ hw5/task5.py
@@ -0,0 +1,18 @@
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
+
+def uniques_inb_order(iterable):
+    for item in iterable:
+        if iterable.count(item) == 1:
+            yield item
+
+
+def uniques_inb_order_adv(iterable):
+    return filter(lambda item: iterable.count(item) == 1, iterable)
+
+
+if __name__ == '__main__':
+    print('Вариант 1:')
+    print([item for item in uniques_inb_order(src)])
+    print('Вариант 2:')
+    print([item for item in uniques_inb_order_adv(src)])
