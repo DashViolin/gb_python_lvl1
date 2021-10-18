4aebcce3b7702887f592c72d571319a49a04a23e
dash <dasha_erminski@mail.ru>
2021-10-18 22:24:55 +0300

    homework_5 (typos correction)

--- hw5/task5.py
+++ hw5/task5.py
@@ -1,13 +1,13 @@
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
 
-def uniques_inb_order(iterable):
+def uniques_in_order(iterable):
     for item in iterable:
         if iterable.count(item) == 1:
             yield item
 
 
-def uniques_inb_order_adv(iterable):
+def uniques_in_order_adv(iterable):
     return filter(lambda item: iterable.count(item) == 1, iterable)
 
 
